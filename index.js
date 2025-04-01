2980907fd97a2c96c9ff11658b19546eb690b642
Darya Buturlakina <darja020302@gmail.com>
2025-04-01 14:31:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express();
-const _ = require('lodash');
-const moment = require('moment');
+const getDate = () => `Hello, World! Time: ${new Date().toLocaleDateString()}`
 
-app.get('/', (req, res) => {
-    res.send(`Hello, World! Time: ${moment().format('YYYY-MM-DD HH:mm:ss')}`);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log(getDate());
